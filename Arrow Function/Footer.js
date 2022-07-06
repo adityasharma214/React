839a592964d0fc5ee1e58021f1ca9d1e841b8f8e
Aditya Sharma <56120068+adityasharma214@users.noreply.github.com>
2022-07-06 16:40:23 +0530

    Create Footer.js
    Function is changed with const. Add = and => instead of function

---
+++ Arrow Function/Footer.js
@@ -0,0 +1,13 @@
+import React from "react";
+// function Footer(){
+//return(
+//<h1>Hello World!</h1>
+//)
+//}
+const Footer = () =>{
+    return(
+        <footer>This is our Footer</footer>
+    )
+}
+
+export default Footer
