698ab4e255e2481cec9f5e77dfd20cc60caf2be4
Aditya Sharma <56120068+adityasharma214@users.noreply.github.com>
2022-07-06 17:11:06 +0530

    Create index.js
    Using conditional statement with react

---
+++ React Conditionals/index.js
@@ -0,0 +1,28 @@
+import React from "react";
+import ReactDOM  from "react-dom";
+
+
+const App = () =>
+{
+    const date = new Date()
+    const hour = date.getHours()
+    let timeofday
+
+    if (hour < 12) {
+        timeofday = "Morning"
+    
+        
+    } else if (hour >= 12 && hour < 12){
+        timeofday = "Noon"
+
+    } else if(hour >15 && hour < 19){
+        timeofday = "Evening"
+    } else{
+        timeofday = "Night"
+    }
+
+return (
+    <h1>Good {timeofday}!</h1>
+)
+}
+ReactDOM.render(<App />, document.getElementById("root"))
