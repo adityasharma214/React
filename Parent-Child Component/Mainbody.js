280c2e4a36d4cb6a5f3215a17871e9cc47147fe2
Aditya Sharma <56120068+adityasharma214@users.noreply.github.com>
2022-07-06 13:59:34 +0530

    Create Mainbody.js

---
+++ Parent-Child Component/Mainbody.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+function Mainbody(){
+    return(
+        <div>
+            <h1>This is my personal blog</h1>
+            <ul>
+                <li>Madhya Pradesh</li>
+                <li>Uttar Pradesh</li>
+                <li>Rajesthan</li>
+            </ul>
+        </div>
+    )
+}
+
+export default Mainbody
