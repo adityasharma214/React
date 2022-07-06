d84518022d019814b8a0cc88d176af07365f10e8
Aditya Sharma <56120068+adityasharma214@users.noreply.github.com>
2022-07-06 14:00:33 +0530

    Create MyInfo.js

---
+++ Parent-Child Component/MyInfo.js
@@ -0,0 +1,20 @@
+import React from "react"
+
+function MyInfo()
+{
+    return(
+        <div>
+            <h1>Aditya Sharma</h1>
+            <p>Hello, I am aditya sharma and we are learning React!</p>
+            <ul>
+                <li>Kedarnath</li>
+                <li>Manali</li>
+                <li>Leh Ladak</li>
+            </ul>
+
+        </div>
+        
+    )
+}
+
+export default MyInfo
