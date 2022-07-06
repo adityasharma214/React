f9194dffd2b2aef1e7b7195dcda988c575f1cfad
Aditya Sharma <56120068+adityasharma214@users.noreply.github.com>
2022-07-06 13:58:21 +0530

    Create Header.js

---
+++ Parent-Child Component/Header.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Header() {
+    return(
+        <div>
+            <h1>Aditya Sharma</h1>
+            <p>This is my personal Blog</p>
+        </div>
+    )
+}
+
+export default Header
