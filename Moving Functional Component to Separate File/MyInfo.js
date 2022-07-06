256d6ad50978e96b3798b36f3bbc7ae8989af113
Aditya Sharma <56120068+adityasharma214@users.noreply.github.com>
2022-07-06 11:45:12 +0530

    Create MyInfo.js
    Functional component created in a separate file.

---
+++ Moving Functional Component to Separate File/MyInfo.js
@@ -0,0 +1,27 @@
+import React from "react"
+
+function MyInfo()
+{
+    return(
+        <div>
+            <h1>Aditya Sharma</h1>
+            <p>Hello, I am aditya sharma and we are learning React!</p>
+            <ul>
+                <li>Kedarnath</li>
+                <li>Manali</li>
+                <li>Leh Ladak</li>
+            </ul>
+
+        </div>
+        
+    )
+}
+
+export default MyInfo
+
+
+// Things to remember - 
+
+// ES6 Export at the end of the component
+
+// This component can be used multiple time, as many times we want.
